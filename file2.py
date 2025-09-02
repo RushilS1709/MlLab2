83affb90435e3c60279b629c045e978bb15d8147
RushilS1709 <bl.en.u4cse23149@bl.students.amrita.edu>
2025-09-02 08:44:31 +0530

    Update file2.py

--- file2.py
+++ file2.py
@@ -8,13 +8,12 @@ excel_file_path = "mllab1\Lab Session Data.xlsx"
 df = pd.read_excel(excel_file_path,sheet_name='Purchase data')
 print(df.head())
 
-df['is_high'] = df['Payment (Rs)'] > 200
-
+df['high'] = df['Payment (Rs)'] > 200
 
 for i in range(len(df)):
     if df.loc[i,"Payment (Rs)"]>200:
-        df.loc[i,"is_high"]="rich"
+        df.loc[i,"high"]="Rich"
     else:
-        df.loc[i,"is_high"]="poor"    
+        df.loc[i,"high"]="Poor"    
 
 print(df)
